f5d438c09884f5baeed34b1f2f651ff9b98c54a3
Cristian <cristianleguizamo31@gmail.com>
2025-08-06 11:27:33 -0500

    Se agrego controlador para manejar la página de presentación, se modifico el html para imprimir una lista y no uno por uno

--- wiki/src/main/java/edu/javeriana/wiki/controllers/MainController.java
+++ wiki/src/main/java/edu/javeriana/wiki/controllers/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-    @RequestMapping("/")
+    @RequestMapping("/description")
     public String description() {
         return "Description";
     }
