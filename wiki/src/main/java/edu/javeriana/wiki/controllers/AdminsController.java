e9ff4f373c29ff31cb61b9f756f3df02a5dabdfa
danielAvila <santi.avila.medina@gmail.com>
2025-08-14 14:40:26 -0500

    feat: Agregada imagen Daniel 🥶

--- wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
+++ wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
@@ -13,9 +13,9 @@ public class AdminsController {
     public String printPresentation(Model model) {
         List<TeamMember> teamMembers = new ArrayList<>();
         teamMembers.add(new TeamMember("Cristian", "CSO", "Java, Python", "Perfil: ","/images/Cristian.png"));
-        teamMembers.add(new TeamMember("Angel", "CTO", "Go, TSX, Java, JS, Python", "Perfil: Una persona apasionada por el desarrollo de software y las lenguas.","/images/angel.jpg"));
-        teamMembers.add(new TeamMember("Daniel", "Arquitecto de Software", "C++, Kotlin, Java, Python", "Perfil: Me gusta hacer diagramas de clases (Soy un psicopata)","imageCristian"));
-        teamMembers.add(new TeamMember("Nicolas", "CIO", "C++, Kotlin, Java, Python, HTML, CSS, JavaScript", "Perfil: Me gusta el manejo y procesamiento de datos por razones aun desconocidas","/images/nicolas.jpg"));
+        teamMembers.add(new TeamMember("Angel", "CTO", "Go, TSX, Java, JS, Python", "Una persona apasionada por el desarrollo de software y las lenguas.","/images/angel.jpg"));
+        teamMembers.add(new TeamMember("Daniel", "Arquitecto de Software", "C++, Kotlin, Java, Python", "Me gusta hacer diagramas de clases (Soy un psicopata)","/images/daniel.jpg"));
+        teamMembers.add(new TeamMember("Nicolas", "CIO", "C++, Kotlin, Java, Python, HTML, CSS, JavaScript", "Me gusta el manejo y procesamiento de datos por razones aun desconocidas","/images/nicolas.jpg"));
         teamMembers.add(new TeamMember("Miguel", "CPO", "Java, Python", "Perfil:","imageCristian"));
         model.addAttribute("teamMembers", teamMembers);
         return "Presentation";
