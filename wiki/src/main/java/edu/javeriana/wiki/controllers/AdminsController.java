fbe70acc5f7fc04f1352d944129db2c900ed3761
Angellovem <aeduardom27@gmail.com>
2025-08-13 23:16:05 -0500

    New design pattern

--- wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
+++ wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
@@ -13,7 +13,7 @@ public class AdminsController {
     public String printPresentation(Model model) {
         List<TeamMember> teamMembers = new ArrayList<>();
         teamMembers.add(new TeamMember("Cristian", "CSO", "Java, Python", "Perfil: ","/images/Cristian.png"));
-        teamMembers.add(new TeamMember("Angel", "CTO", "Java, Python", "Perfil:","imageCristian"));
+        teamMembers.add(new TeamMember("Angel", "CTO", "Go, TSX, Java, JS, Python", "Perfil: Una persona apasionada por el desarrollo de software y las lenguas.","/images/angel.jpg"));
         teamMembers.add(new TeamMember("Daniel", "Arquitecto de Software", "C++, Kotlin, Java, Python", "Perfil: Me gusta hacer diagramas de clases (Soy un psicopata)","imageCristian"));
         teamMembers.add(new TeamMember("Nicolas", "CIO", "C++, Kotlin, Java, Python, HTML, CSS, JavaScript", "Perfil: Me gusta el manejo y procesamiento de datos por razones aun desconocidas","/images/nicolas.jpg"));
         teamMembers.add(new TeamMember("Miguel", "CPO", "Java, Python", "Perfil:","imageCristian"));
