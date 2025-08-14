b92ae2985e1a26f60919a90ea9cb3c54f6b099b2
Cristian <cristianleguizamo31@gmail.com>
2025-08-13 20:55:32 -0500

    Página de presentación con fotos

--- wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
+++ wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
@@ -8,15 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminsController {
-    public record TeamMember(String name, String role, String languages, String profile){}
+    public record TeamMember(String name, String role, String languages, String profile, String imageUrl){}
     @GetMapping("/presentation")
     public String printPresentation(Model model) {
         List<TeamMember> teamMembers = new ArrayList<>();
-        teamMembers.add(new TeamMember("Cristian", "CSO", "Java, Python", "Perfil: "));
-        teamMembers.add(new TeamMember("Angel", "CTO", "Java, Python", "Perfil:"));
-        teamMembers.add(new TeamMember("Daniel", "Arquitecto de Software", "C++, Kotlin, Java, Python", "Perfil: Me gusta hacer diagramas de clases (Soy un psicopata)"));
-        teamMembers.add(new TeamMember("Nicolas", "CIO", "Java, Python", "Perfil:"));
-        teamMembers.add(new TeamMember("Miguel", "CPO", "Java, Python", "Perfil:"));
+        teamMembers.add(new TeamMember("Cristian", "CSO", "Java, Python", "Perfil: ","/images/Cristian.png"));
+        teamMembers.add(new TeamMember("Angel", "CTO", "Java, Python", "Perfil:","imageCristian"));
+        teamMembers.add(new TeamMember("Daniel", "Arquitecto de Software", "C++, Kotlin, Java, Python", "Perfil: Me gusta hacer diagramas de clases (Soy un psicopata)","imageCristian"));
+        teamMembers.add(new TeamMember("Nicolas", "CIO", "Java, Python", "Perfil:","imageCristian"));
+        teamMembers.add(new TeamMember("Miguel", "CPO", "Java, Python", "Perfil:","imageCristian"));
         model.addAttribute("teamMembers", teamMembers);
         return "Presentation";
     }
