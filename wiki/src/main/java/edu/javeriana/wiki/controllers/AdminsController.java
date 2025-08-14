116ced7a190e76aae8df1b3a3adf42e0123ae9d8
Exmator <151688577+Exmator@users.noreply.github.com>
2025-08-14 15:07:42 -0500

    ADD: Miguel info

--- wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
+++ wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
@@ -16,7 +16,7 @@ public class AdminsController {
         teamMembers.add(new TeamMember("Angel", "CTO", "Go, TSX, Java, JS, Python", "Una persona apasionada por el desarrollo de software y las lenguas.","/images/angel.jpg"));
         teamMembers.add(new TeamMember("Daniel", "Arquitecto de Software", "C++, Kotlin, Java, Python", "Me gusta hacer diagramas de clases (Soy un psicopata)","/images/daniel.jpg"));
         teamMembers.add(new TeamMember("Nicolas", "CIO", "C++, Kotlin, Java, Python, HTML, CSS, JavaScript", "Me gusta el manejo y procesamiento de datos por razones aun desconocidas","/images/nicolas.jpg"));
-        teamMembers.add(new TeamMember("Miguel", "CPO", "Java, Python", "Perfil:","imageCristian"));
+        teamMembers.add(new TeamMember("Miguel", "CPO", "Java, Python", "Rey del mambo","/images/miguel.jpg"));
         model.addAttribute("teamMembers", teamMembers);
         return "Presentation";
     }
