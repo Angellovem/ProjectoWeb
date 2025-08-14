28869eca8c9c996b626467932be8cf565fff0ffb
Cristian <cristianleguizamo31@gmail.com>
2025-08-14 17:02:56 -0500

    Perfil

--- wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
+++ wiki/src/main/java/edu/javeriana/wiki/controllers/AdminsController.java
@@ -12,7 +12,7 @@ public class AdminsController {
     @GetMapping("/presentation")
     public String printPresentation(Model model) {
         List<TeamMember> teamMembers = new ArrayList<>();
-        teamMembers.add(new TeamMember("Cristian", "Líder Técnico", "Java, Python", "Perfil: Motivado por el bienestar de los clientes y el trabajo en equipo ","/images/Cristian.png"));
+        teamMembers.add(new TeamMember("Cristian", "Líder Técnico", "C++, JS, CSS, Go, Java, Python.", "Perfil: Motivado por el bienestar de los clientes y el trabajo en equipo ","/images/Cristian.png"));
         teamMembers.add(new TeamMember("Angel", "CTO", "Go, TSX, Java, JS, Python", "Una persona apasionada por el desarrollo de software y las lenguas.","/images/angel.jpg"));
         teamMembers.add(new TeamMember("Daniel", "Arquitecto de Software", "C++, Kotlin, Java, Python", "Me gusta hacer diagramas de clases (Soy un psicopata)","/images/daniel.jpg"));
         teamMembers.add(new TeamMember("Nicolas", "CIO", "C++, Kotlin, Java, Python, HTML, CSS, JavaScript", "Me gusta el manejo y procesamiento de datos por razones aun desconocidas","/images/nicolas.jpg"));
